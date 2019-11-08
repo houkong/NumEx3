097a8983410ad468746c24f5a895b8c13055d720
houkong <37408284+houkong@users.noreply.github.com>
2019-11-08 11:05:41 +0100

    Version 3
    Added poincaré section

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Pendulum import*
 import matplotlib.pyplot as plt
 
-
+"""
 pendel = Pendulum.with_rk4(stop=60, step=0.01)
 
 t, theta, _ = pendel(F_D = 1.2)
@@ -9,4 +9,17 @@ t2, theta2, _ = pendel(theta0=0.21)
 
 plt.plot(t, theta)
 plt.plot(t2, theta2)
+plt.show()
+"""
+
+STEP=0.01
+
+t, theta, omega = Pendulum.with_euler_cromer(stop=40000, step=STEP)(F_D=1.2)
+thetap, omegap = [], []
+for i, t_i in enumerate(t):
+    if abs(t_i%(2*np.pi/params.Omega_D))<STEP:
+        thetap.append(theta[i])
+        omegap.append(omega[i])
+
+plt.plot(thetap, omegap, 'b.')
 plt.show()
